7284c041362df3a8878b71974c1f8b84c049b3a9
TinyBlondMan <nicolas.s.humbert@gmail.com>
2023-07-16 22:43:12 +0200

    Added db context and sqlite db;

---
+++ Data/Employee.cs
@@ -0,0 +1,12 @@
+namespace ecocivicom_blazor_clone.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Department { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
